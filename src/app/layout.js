aaa4e0ace06e1e6f221f8161f37db0f904414faa
kbinreallife <kbinreallife@gmail.com>
2025-11-15 17:03:24 -0800

    update imports and use toggle provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SectionToggleProvider } from "../context/SectionToggleContext";
+import SectionToggleClientProvider from "./SectionToggleClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SectionToggleProvider>
+        <SectionToggleClientProvider>
           {children}
-        </SectionToggleProvider>
+        </SectionToggleClientProvider>
       </body>
     </html>
   );
