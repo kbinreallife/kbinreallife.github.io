aa553023f446fcbecd74766fd86e928f9e09ddfe
kbinreallife <kbinreallife@gmail.com>
2025-11-15 17:38:05 -0800

    missed anotha 1

--- src/app/SectionToggleClientProvider.js
+++ src/app/SectionToggleClientProvider.js
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+import React from "react";
 import { SectionToggleProvider } from '../context/SectionToggleContext';
 
 export default function SectionToggleClientProvider({ children }) {
